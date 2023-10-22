0d2f52ed0fd178bb97a065a9f7da8b719c0d9213
rcmnrl <ricagayoma@gmail.com>
2023-10-23 05:28:50 +0800

    Counter

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Header from "./Components/Header";
+import Counter from "./Components/Counter";
+import Footer from "./Components/Footer";
+import './Components/App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="background d-flex flex-column align-items-center justify-content-center">
+      <div className="container">
+        <Header title="Using React" count={count} />
+        <Counter setCount={setCount} />
+      </div>
+      <Footer copyright="© rcmnrl 2023" className="footer mt-3" />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'; // Import your CSS file
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCounter = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="form-box">
+        <div className="counter">
+          <h1 className="text-primary fw-bold">
+            Counter {count}
+          </h1>
+
+          <div className="btn-group">
+            <button onClick={incrementCounter} className="btn btn-primary me-2">
+              Increment
+            </button>
+            <button onClick={decrementCounter} className="btn btn-danger">
+              Decrement
+            </button>
+          </div>
+
+          {count > 5 ? (
+            <h5 className="text-success">Congratulations! Counter is greater than 5.</h5>
+          ) : (
+            <h5 className="not-yet text-danger">Not yet</h5>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
